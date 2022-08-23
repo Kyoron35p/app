497f78927f81daaa0ba18b0412c59878a46fad0a
kyoron <3367202209@qq.com>
2022-08-23 23:25:57 +0800

    在家学习的，调整了半天bug，只添加了静态组件的头部底部

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <!-- <Header></Header> -->
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 // 引入Header组件
-// import Header from './components/Header';
-
+import Header from './components/Header';
+// 引入Footer组件
+import Footer from './components/Footer';
 export default {
   name: 'App',
   components:{
-    // Header
+    Header,
+    Footer
   }
 }
 </script>
--- app/vue.config.js
+++ app/vue.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭ESlint代码校验
-  lintOnSave: false
+  lintOnSave: false,
+  // run项目之后直接在浏览器中打开
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080
+  }
 })
