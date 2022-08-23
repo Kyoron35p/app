67a289ae2228b5440e503e3f23f701923785cb10
Kyoron <Kyoron35p@163.com>
2022-08-23 17:03:51 +0800

    关闭ESlint的代码校验以及检查项目配置

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭ESlint代码校验
+  lintOnSave: false
 })
